116b1ffe29cf67bcd457be23c40a52be052a98e6
Owoeye precious <owoeyeprecious36@gmail.com>
2023-07-04 02:39:49 +0100

    PunkNft contract crated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-etherscan");
 
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_API_KEY
+  }
 };
